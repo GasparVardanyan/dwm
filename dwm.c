9277e38b0adf8ee73cfbaa005dbf086f9dfc9b1b
GasparVardanyan <gaspar_pm@proton.me>
2024-09-02 01:01:12 +0400

    movecenter: fix the crash when no window is selected

--- dwm.c
+++ dwm.c
@@ -1206,9 +1206,11 @@ movemouse(const Arg *arg)
 void
 movecenter(const Arg *arg)
 {
-	selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
-	selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
-	arrange(selmon);
+	if (selmon->sel) {
+		selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
+		selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
+		arrange(selmon);
+	}
 }
 
 Client *
